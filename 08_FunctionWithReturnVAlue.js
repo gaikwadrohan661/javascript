2b73b28105963fe7f968fceeb60cde7759981780
gaikwadrohan661 <gaikwadrohan661@gmail.com>
2023-05-17 08:35:20 +0530

    function
    function

---
+++ 08_FunctionWithReturnVAlue.js
@@ -0,0 +1,21 @@
+function addition(num1, num2) {
+  // console.log("Addition of number is :", num1+num2);
+
+  result = num1 + num2;
+  return result;
+}
+var ansvalue = addition(100, 200);
+console.log("Addition oftwo number is:", ansvalue);
+function addition(num1, num2) {
+  var result = num1 + num2; // result = 300
+
+  return result;
+}
+
+var returnValue = addition(100, 200);
+
+console.log("Addition of number is: ", returnValue);
+
+var returnResult = addition(890.4, 600.3456);
+
+console.log("Addition of number is: ", returnResult);
---
+++ 09_string.js
@@ -0,0 +1,32 @@
+
+var rohan="Good Morning";
+var rohanLength=rohan.length;
+console.log("String length is :",rohanLength);
+ var charAtZaroindex = rohan.charAt(0);
+ console.log("Char at zero index is:",charAtZaroindex);
+  var charAtlast = rohan.charAt(rohan.length-1);
+  console.log("Char at last is:",charAtlast);
+
+  var indexOfr = rohan.indexOf("r");
+  console.log("Index of r is:",indexOfr);
+
+  var replaceString = rohan.replace("Morning","Afternoon");
+  console.log("After replac string is:",replaceString);
+
+  var upperCase = rohan.toUpperCase();
+  console.log("Uppercase string is:",upperCase);
+
+
+var fullName="Rohan Dipakraj Gaikwad";
+var fullNameLength = fullName.length;
+console.log("Total number of char in my full name is:",fullNameLength);
+
+var firstName = "   Rohan   ";
+var trimmedfirstName = firstName.trim();
+console.log("trimmed first name string is :",trimmedfirstName ,trimmedfirstName.length);
+
+var firstName = "Rohan";
+var LastName = "Gaikwad";
+
+var concatstring = firstName.concat(LastName);
+console.log("Concat string is:",concatstring);
